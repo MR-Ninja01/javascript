4254c19214315f20ee819eb24a4525de4a62b1bc
Harshit_dev <legendp661@gmail.com>
2025-09-22 20:40:12 +0530

    First push from E drive

---
+++ datatype.js
@@ -0,0 +1,13 @@
+/*  in js have 7 datatype {N N S S B B U}
+N=Null   in which value not assign   type= object \standlone value
+N=numnber in only integer value 
+S=string  - in string sentences
+S=symbol   -in symbols 
+B=Bigint   - Large size of integer
+B=boolean   - it has only true/ false.
+U =undefined  in which value not assign ...
+*/ 
+
+
+// conversion
+
---
+++ vararible.js
@@ -0,0 +1,14 @@
+// variable are three types in js var || const || let .
+
+var a=10;
+console.log(a)
+
+// var can declare multiple times but no let 
+var b=4
+var b=a
+console.log(b)
+
+
+const c=15
+// c=12   cant be changed 
+console.log(c)
